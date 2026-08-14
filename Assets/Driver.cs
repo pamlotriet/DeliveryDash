94a75e24c981f05ff154569a38b87e4d1f448e83
pamela lotriet <pamelalotriet@gmail.com>
2026-08-14 11:34:00 +0200

    added collision detection

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -21,28 +21,24 @@ public class Driver : MonoBehaviour
             move = 1f;
             moveAmount = move * moveSpeed * Time.deltaTime;
             MoveCarForward(moveAmount);
-            Debug.Log("W key is pressed");
         }
         else if (Keyboard.current.sKey.isPressed)
         {
             move = 1f;
             moveAmount = move * moveSpeed * Time.deltaTime;
             MoveCarBackward(moveAmount);
-            Debug.Log("S key is pressed");
         }
         if (Keyboard.current.aKey.isPressed)
         {
             steer = 1f;
             steerAmount = steer * steerSpeed * Time.deltaTime;
             SteerLeft(steerAmount);
-            Debug.Log("A key is pressed");
         }
         else if (Keyboard.current.dKey.isPressed)
         {
             steer = 1f;
             steerAmount = steer * steerSpeed * Time.deltaTime;
             SteerRight(steerAmount);
-            Debug.Log("D key is pressed");
         }
     }
     void MoveCarForward(float moveAmount)
